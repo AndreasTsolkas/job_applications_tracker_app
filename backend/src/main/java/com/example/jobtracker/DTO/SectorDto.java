07cfb81e8dcb87ac064047f952a63b2d535cc334
Andrew Tsolkas <tsolkasadreas@gmail.com>
2026-07-29 17:35:49 +0300

    Adds "Sector" DTO

---
+++ backend/src/main/java/com/example/jobtracker/DTO/SectorDto.java
@@ -0,0 +1,22 @@
+package com.example.jobtracker.DTO;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SectorDto {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
