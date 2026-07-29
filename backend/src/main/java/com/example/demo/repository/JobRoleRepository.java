4c60b0d05d35d329fff74ec9577487e91076fb15
Andrew Tsolkas <tsolkasadreas@gmail.com>
2026-07-29 13:22:44 +0300

    Adds "JobRole" repository

---
+++ backend/src/main/java/com/example/demo/repository/JobRoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.JobRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JobRoleRepository extends JpaRepository<JobRole, Long> {
+
+    Optional<JobRole> findByName(String name);
+}
