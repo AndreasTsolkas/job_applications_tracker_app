b4f62c8a552b2b6d26b52bb42ad6ae929ec131b4
Andrew Tsolkas <tsolkasadreas@gmail.com>
2026-07-29 17:42:58 +0300

    Adds "EmploymentType" DTO

---
+++ backend/src/main/java/com/example/jobtracker/DTO/EmploymentTypeDTO.java
@@ -0,0 +1,23 @@
+package com.example.jobtracker.DTO;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmploymentTypeDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
