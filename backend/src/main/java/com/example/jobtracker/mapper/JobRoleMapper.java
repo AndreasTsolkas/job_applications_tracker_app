98473783eb558d5586106bde29b3accaa177e503
Andrew Tsolkas <tsolkasadreas@gmail.com>
2026-07-30 11:59:51 +0300

    Adds "JobRole" mapper

---
+++ backend/src/main/java/com/example/jobtracker/mapper/JobRoleMapper.java
@@ -0,0 +1,41 @@
+package com.example.jobtracker.mapper;
+
+import com.example.jobtracker.DTO.JobRoleDTO;
+import com.example.jobtracker.entity.JobRole;
+
+public class JobRoleMapper {
+
+    private JobRoleMapper() {
+    }
+
+    public static JobRoleDTO toDTO(JobRole jobRole) {
+
+        if (jobRole == null) {
+            return null;
+        }
+
+        return JobRoleDTO.builder()
+                .id(jobRole.getId())
+                .name(jobRole.getName())
+                .description(jobRole.getDescription())
+                .createdAt(jobRole.getCreatedAt())
+                .updatedAt(jobRole.getUpdatedAt())
+                .build();
+    }
+
+
+    public static JobRole toEntity(JobRoleDTO dto) {
+
+        if (dto == null) {
+            return null;
+        }
+
+        return JobRole.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .createdAt(dto.getCreatedAt())
+                .updatedAt(dto.getUpdatedAt())
+                .build();
+    }
+}
