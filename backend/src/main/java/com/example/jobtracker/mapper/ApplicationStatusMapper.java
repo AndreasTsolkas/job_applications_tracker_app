c9e6a0a069e089f9a1ecddf9d9b4416e156c6fad
Andrew Tsolkas <tsolkasadreas@gmail.com>
2026-07-30 12:22:38 +0300

    Adds "ApplicationStatus" mapper

---
+++ backend/src/main/java/com/example/jobtracker/mapper/ApplicationStatusMapper.java
@@ -0,0 +1,35 @@
+package com.example.jobtracker.mapper;
+
+import com.example.jobtracker.DTO.ApplicationStatusDTO;
+import com.example.jobtracker.entity.ApplicationStatus;
+
+public class ApplicationStatusMapper {
+
+    private ApplicationStatusMapper() {
+    }
+
+    public static ApplicationStatusDTO toDTO(ApplicationStatus applicationStatus) {
+
+        if (applicationStatus == null) {
+            return null;
+        }
+
+        return ApplicationStatusDTO.builder()
+                .id(applicationStatus.getId())
+                .name(applicationStatus.getName())
+                .build();
+    }
+
+
+    public static ApplicationStatus toEntity(ApplicationStatusDTO dto) {
+
+        if (dto == null) {
+            return null;
+        }
+
+        return ApplicationStatus.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .build();
+    }
+}
