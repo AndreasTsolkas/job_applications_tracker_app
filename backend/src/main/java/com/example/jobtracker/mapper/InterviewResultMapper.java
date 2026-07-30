4d52e0c8ca33a6973a82835fc3bbd30a28cfb041
Andrew Tsolkas <tsolkasadreas@gmail.com>
2026-07-30 13:40:19 +0300

    Adds "InterviewResult" mapper

---
+++ backend/src/main/java/com/example/jobtracker/mapper/InterviewResultMapper.java
@@ -0,0 +1,35 @@
+package com.example.jobtracker.mapper;
+
+import com.example.jobtracker.DTO.InterviewResultDTO;
+import com.example.jobtracker.entity.InterviewResult;
+
+public class InterviewResultMapper {
+
+    private InterviewResultMapper() {
+    }
+
+    public static InterviewResultDTO toDTO(InterviewResult interviewResult) {
+
+        if (interviewResult == null) {
+            return null;
+        }
+
+        return InterviewResultDTO.builder()
+                .id(interviewResult.getId())
+                .name(interviewResult.getName())
+                .build();
+    }
+
+
+    public static InterviewResult toEntity(InterviewResultDTO dto) {
+
+        if (dto == null) {
+            return null;
+        }
+
+        return InterviewResult.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .build();
+    }
+}
